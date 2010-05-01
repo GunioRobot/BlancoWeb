422c7e7511947c427e897e130074e67477e7135a
Javier Cordero <jneight@gmail.com>
2010-05-01 17:13:34 +0200

    bugfix rss

---
+++ web/feeds.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.contrib.syndication.feeds import Feed;
+from web.models import Evento;
+
+class EventoFeed(Feed):
+    title = "Blanco Irish Tavern";
+    link = "/evento/";
+    description = "Suscríbete para conocer todas nuestras fiestas"
+    
+    def items(self):
+        return Evento.objects.all().order_by("-fecha");
+    
+    def item_title(self, item):
+        return item.nombre
+    
+    def item_description(self, item):
+        descripcion = "%s" % item.fecha;
+        descripcion += " %s" % item.hora_inicio;
+        descripcion += " %s" % item.info;
+        return descripcion;
